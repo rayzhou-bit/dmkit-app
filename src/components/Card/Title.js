48bd1cbdaf559ca3d0a8157c7c6bbd30b030f960
Raymond Zhou <56318341+rayzhou-bit@users.noreply.github.com>
2023-05-08 14:13:03 -0700

    action dropdown (#17)

--- src/components/Card/Title.js
+++ src/components/Card/Title.js
@@ -1,13 +1,17 @@
 import React, { useState, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 
-import * as actions from '../../store/actionIndex';
-import TitleInput from '../UI/Inputs/TitleInput';
-
 import './index.scss';
 import '../../styles/colors.scss';
 import { LightColors } from '../../styles/colors';
+import LibraryIcon from '../../assets/icons/library-icon.png';
+import RedTrashIcon from '../../assets/icons/red-trash.png';
+
+import * as actions from '../../store/actionIndex';
+
+import TitleInput from '../UI/Inputs/TitleInput';
 import ColorDropdown from '../../sharedComponents/Dropdowns/ColorDropdown';
+import ActionDropdown from '../../sharedComponents/Dropdowns/ActionDropdown';
 
 const Title = ({
   cardId,
@@ -27,23 +31,60 @@ const Title = ({
 
   const isLightColor = LightColors.includes(color);
 
+  const options = [
+    // {
+    //   title: 'Insert image',
+    //   callback: () => {},
+    // },
+    {
+      title: 'Duplicate card',
+      callback: () => dispatch(actions.copyCard(cardId)),
+    },
+    {},
+    {
+      title: 'Rename',
+      callback: () => {},
+    },
+    {},
+    // {
+    //   title: 'Bring to front',
+    //   callback: () => {},
+    // },
+    // {
+    //   title: 'Send to back',
+    //   callback: () => {},
+    // },
+    {
+      title: 'Move to unsorted',
+      callback: () => {},
+      icon: LibraryIcon,
+    },
+    {},
+    {
+      title: 'Delete',
+      type: 'danger',
+      callback: () => {},
+      icon: RedTrashIcon,
+    },
+  ];
+
   return (
     <div
-      className={"title " + color}
+      className={'title ' + color}
     >
       <TitleInput
-        className={"input-div" + (isLightColor ? " dark" : " light")}
+        className={'input-div' + (isLightColor ? ' dark' : ' light')}
         saveValue={(value) => dispatch(actions.updCardTitle(cardId, value))}
         setEditingParent={setEditingCard}
-        type="card" 
+        type='card'
         value={title} 
       />
       <button
-        className="title-btn"
+        className='title-btn'
         ref={colorDropdownBtnRef}
         onClick={() => setIsColorDropdownOpen(!isColorDropdownOpen)}
       >
-        <i className={"open-color-icon" + (isLightColor ? " dark" : " light")} />
+        <i className={'open-color-icon' + (isLightColor ? ' dark' : ' light')} />
       </button>
       <ColorDropdown
         btnRef={colorDropdownBtnRef}
@@ -53,11 +94,18 @@ const Title = ({
         updateColor={(color) => dispatch(actions.updCardColor(cardId, color))}
       />
       <button
-        className="title-btn"
-        // onClick={() => dispatch(actions.unlinkCardFromView(cardId))}
+        className='title-btn'
+        ref={optionDropdownBtnRef}
+        onClick={() => setIsOptionDropdownOpen(!isOptionDropdownOpen)}
       >
-        <i className={"open-dropdown-icon" + (isLightColor ? " dark" : " light")} />
+        <i className={'open-dropdown-icon' + (isLightColor ? ' dark' : ' light')} />
       </button>
+      <ActionDropdown
+        btnRef={optionDropdownBtnRef}
+        closeDropdown={() => setIsOptionDropdownOpen(false)}
+        isOpen={isOptionDropdownOpen}
+        items={options}
+      />
     </div>
   );
 };
