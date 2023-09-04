85721402e5ed144b7e03f20c2f48cb20a76a4c94
Mark <marktrepanier.cajigas@gmail.com>
2023-09-04 17:17:52 -0400

    applies "roboto" to library card

--- src/components/Card/Title.js
+++ src/components/Card/Title.js
@@ -1,24 +1,19 @@
-import React, { useState, useRef } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState, useRef } from "react";
+import { useDispatch } from "react-redux";
 
-import './index.scss';
-import '../../styles/colors.scss';
-import { LightColors } from '../../styles/colors';
-import LibraryIcon from '../../assets/icons/library-icon.png';
-import RedTrashIcon from '../../assets/icons/red-trash.png';
+import "./index.scss";
+import "../../styles/colors.scss";
+import { LightColors } from "../../styles/colors";
+import LibraryIcon from "../../assets/icons/library-icon.png";
+import RedTrashIcon from "../../assets/icons/red-trash.png";
 
-import * as actions from '../../store/actionIndex';
+import * as actions from "../../store/actionIndex";
 
-import TitleInput from '../UI/Inputs/TitleInput';
-import ColorDropdown from '../../sharedComponents/Dropdowns/ColorDropdown';
-import ActionDropdown from '../../sharedComponents/Dropdowns/ActionDropdown';
+import TitleInput from "../UI/Inputs/TitleInput";
+import ColorDropdown from "../../sharedComponents/Dropdowns/ColorDropdown";
+import ActionDropdown from "../../sharedComponents/Dropdowns/ActionDropdown";
 
-const Title = ({
-  cardId,
-  color,
-  setEditingCard,
-  title,
-}) => {
+const Title = ({ cardId, color, setEditingCard, title }) => {
   const dispatch = useDispatch();
 
   // STATES
@@ -37,12 +32,12 @@ const Title = ({
     //   callback: () => {},
     // },
     {
-      title: 'Duplicate card',
+      title: "Duplicate card",
       callback: () => dispatch(actions.copyCard(cardId)),
     },
     {},
     {
-      title: 'Rename',
+      title: "Rename",
       callback: () => {},
     },
     {},
@@ -55,36 +50,36 @@ const Title = ({
     //   callback: () => {},
     // },
     {
-      title: 'Move to unsorted',
+      title: "Move to unsorted",
       callback: () => {},
       icon: LibraryIcon,
     },
     {},
     {
-      title: 'Delete',
-      type: 'danger',
+      title: "Delete",
+      type: "danger",
       callback: () => {},
       icon: RedTrashIcon,
     },
   ];
 
   return (
-    <div
-      className={'title ' + color}
-    >
+    <div className={"title " + color}>
       <TitleInput
-        className={'input-div' + (isLightColor ? ' dark' : ' light')}
+        className={"input-div" + (isLightColor ? " dark" : " light")}
         saveValue={(value) => dispatch(actions.updCardTitle(cardId, value))}
         setEditingParent={setEditingCard}
-        type='card'
-        value={title} 
+        type="card"
+        value={title}
       />
       <button
-        className='title-btn'
+        className="title-btn"
         ref={colorDropdownBtnRef}
         onClick={() => setIsColorDropdownOpen(!isColorDropdownOpen)}
       >
-        <i className={'open-color-icon' + (isLightColor ? ' dark' : ' light')} />
+        <i
+          className={"open-color-icon" + (isLightColor ? " dark" : " light")}
+        />
       </button>
       <ColorDropdown
         btnRef={colorDropdownBtnRef}
@@ -94,11 +89,13 @@ const Title = ({
         updateColor={(color) => dispatch(actions.updCardColor(cardId, color))}
       />
       <button
-        className='title-btn'
+        className="title-btn"
         ref={optionDropdownBtnRef}
         onClick={() => setIsOptionDropdownOpen(!isOptionDropdownOpen)}
       >
-        <i className={'open-dropdown-icon' + (isLightColor ? ' dark' : ' light')} />
+        <i
+          className={"open-dropdown-icon" + (isLightColor ? " dark" : " light")}
+        />
       </button>
       <ActionDropdown
         btnRef={optionDropdownBtnRef}
