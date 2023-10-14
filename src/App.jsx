1781395e7dfdd35b411dca3dd0edefdf2c5a59a6
Jason Chen <jason.l.chen@gmail.com>
2023-10-14 16:14:25 -0700

    renamed ViewScreen to Canvas and relocated files

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,31 @@ import UserMenu from './components/UserMenu/UserMenu';
 import ToolMenu from './components/ToolMenu/ToolMenu';
 import Library from './components/Library/Library';
 import ViewSelect from './components/ViewSelect/ViewSelect';
-import ViewScreen from './components/ViewScreen/ViewScreen';
+import Canvas from './containers/Canvas/Canvas';
 import Popup from './sharedComponents/Popup';
 import { resetPopup } from './store/actionIndex';
 
 const App = () => {
   const dispatch = useDispatch();
-  const popup = useSelector(state => state.sessionManager.popup);
+  const popup = useSelector((state) => state.sessionManager.popup);
 
   // REFS
   const toolMenuRef = useRef();
 
   // Disable scrolling
-  document.body.scroll = "no";
+  document.body.scroll = 'no';
   document.body.style.backgroundColor = 'gray';
   document.body.style.overflow = 'hidden';
   document.height = window.innerHeight;
 
   return (
-    <div className="layout">
+    <div className='layout'>
       <Loader />
       <UserMenu />
       <ToolMenu toolMenuRef={toolMenuRef} />
       <Library />
       <ViewSelect />
-      <ViewScreen toolMenuRef={toolMenuRef} />
+      <Canvas toolMenuRef={toolMenuRef} />
       <Popup resetCallback={() => dispatch(resetPopup())} type={popup.type} />
     </div>
   );
