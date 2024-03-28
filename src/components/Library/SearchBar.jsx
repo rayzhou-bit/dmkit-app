d68ccefee3f6ce0b43a836a508aa4dfb6821e738
unknown <marktrepanier.cajigas@gmail.com>
2024-03-28 19:23:56 -0400

    removes search-count and fixes searchImg

--- src/components/Library/SearchBar.jsx
+++ src/components/Library/SearchBar.jsx
@@ -1,22 +1,25 @@
 import { useState } from "react";
 
+import SearchImg from '../../assets/icons/search.png';
+
 const SearchBar = ( setSearchString ) => {
     const [enteredSearch, setEnteredSearch] = useState('');
 
     return(
-        <>        
-            <img src={SearchImg} alt="Search" />
+        <>   
             <div className="search-bar">
+                     
+                <img src={SearchImg} alt="Search" />
                 <input className="search-input"
                 type="search" placeholder="Search..."
                 value={enteredSearch} onChange={e => setEnteredSearch(e.target.value)}
                 />
-                <div className="search-count">
+                {/* <div className="search-count">
                 {(cardList.length) + "/" + (cardCollection ? Object.keys(cardCollection).length : "")}
-                </div>
+                </div> */}
             </div>
         </>
     )
-}
+};
 
 export default SearchBar;
