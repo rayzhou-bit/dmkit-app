b28f70e71d22f446cc2b4a20ba3f55e4d77ff3e5
unknown <marktrepanier.cajigas@gmail.com>
2024-03-28 18:47:46 -0400

    copies searchBar code from librarySearch

--- src/components/Library/SearchBar.jsx
+++ src/components/Library/SearchBar.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
 const SearchBar = ( setSearchString ) => {
-    console.log(setSearchString || "sanity")
+    const [enteredSearch, setEnteredSearch] = useState('');
+
+    return(
+        <>        
+            <img src={SearchImg} alt="Search" />
+            <div className="search-bar">
+                <input className="search-input"
+                type="search" placeholder="Search..."
+                value={enteredSearch} onChange={e => setEnteredSearch(e.target.value)}
+                />
+                <div className="search-count">
+                {(cardList.length) + "/" + (cardCollection ? Object.keys(cardCollection).length : "")}
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default SearchBar;
