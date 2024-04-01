31c6f957ffe24b4648ff04d6b70f0d9f17cf1ab0
unknown <marktrepanier.cajigas@gmail.com>
2024-04-01 19:07:23 -0400

    changes placeholder text and classnames

--- src/components/Library/SearchBar.jsx
+++ src/components/Library/SearchBar.jsx
@@ -1,24 +1,24 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
+import './LibrarySearch.scss';
 import SearchImg from '../../assets/icons/search.png';
 
 const SearchBar = ( setSearchString ) => {
     const [enteredSearch, setEnteredSearch] = useState('');
 
     return(
-        <>   
-            <div className="search-bar">
-                     
+        <div className="new-library-search-bar-container">   
+            <div className="search-bar">    
                 <img src={SearchImg} alt="Search" />
                 <input className="search-input"
-                type="search" placeholder="Search..."
+                type="search" placeholder="Search Library"
                 value={enteredSearch} onChange={e => setEnteredSearch(e.target.value)}
                 />
                 {/* <div className="search-count">
                 {(cardList.length) + "/" + (cardCollection ? Object.keys(cardCollection).length : "")}
                 </div> */}
             </div>
-        </>
+        </div>
     )
 };
 
