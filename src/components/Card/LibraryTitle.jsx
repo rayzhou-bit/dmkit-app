f71d7b13f8dc8209bbaefcadcf020f4e8e405d4a
Raymond Zhou <56318341+rayzhou-bit@users.noreply.github.com>
2024-04-26 01:17:21 -0700

    library fixes (#45)

---
+++ src/components/Card/LibraryTitle.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+
+import { useTitleHooks, useColorDropdownHooks, useOptionsDropdownLibraryHooks } from './hooks';
+
+import ColorDropdown from '../../components-shared/Dropdowns/ColorDropdown';
+import ActionDropdown from '../../components-shared/Dropdowns/ActionDropdown';
+
+import './Card.scss';
+import '../../styles/colors.scss';
+import OpenColorBlackIcon from '../../assets/icons/rounded-square.svg';
+import OpenColorWhiteIcon from '../../assets/icons/rounded-square-white.svg';
+import DropdownArrowBlackIcon from '../../assets/icons/dropdown-arrow.svg';
+import DropdownArrowWhiteIcon from '../../assets/icons/dropdown-arrow-white.svg';
+
+const LibraryTitle = ({
+  cardId,
+  setEditingCard,
+}) => {
+
+  const {
+    inputClassName,
+    readOnly,
+    titleRef,
+    titleValue,
+    changeTitleValue,
+    beginTitleEdit,
+    endTitleEdit,
+    handleTitleKeyPress,
+  } = useTitleHooks({
+    cardId,
+    setEditingCard,
+  });
+
+  const {
+    color,
+    colorDropdownBtnRef,
+    isColorDropdownOpen,
+    isLightColor,
+    closeColorDropdown,
+    openColorDropdown,
+    updateColor,
+  } = useColorDropdownHooks({ cardId });
+
+  const {
+    isOptionDropdownOpen,
+    options,
+    optionDropdownBtnRef,
+    closeOptionsDropdown,
+    openOptionsDropdown,
+  } = useOptionsDropdownLibraryHooks({
+    beginTitleEdit,
+    cardId,
+  });
+
+  return (
+    <div className={'title ' + color}>
+      <div className={'input-div' + (isLightColor ? ' dark' : ' light')}>
+        <input
+          className={inputClassName}
+          maxLength='50'
+          onBlur={endTitleEdit}
+          onChange={(e) => changeTitleValue(e.target.value)}
+          onDoubleClick={beginTitleEdit}
+          onDragOver={(e) => e.preventDefault()}
+          onKeyDown={handleTitleKeyPress}
+          readOnly={readOnly}
+          ref={titleRef}
+          title={titleValue}
+          type='text'
+          value={titleValue}
+        />
+      </div>
+      <button
+        className='color-btn'
+        onClick={openColorDropdown}
+        ref={colorDropdownBtnRef}
+      >
+        <img src={isLightColor ? OpenColorBlackIcon : OpenColorWhiteIcon} />
+      </button>
+      <ColorDropdown
+        cardId={cardId}
+        btnRef={colorDropdownBtnRef}
+        cardColor={color}
+        isOpen={isColorDropdownOpen}
+        onClose={closeColorDropdown}
+        onUpdateColor={updateColor}
+      />
+      <button
+        className='dropdown-btn'
+        ref={optionDropdownBtnRef}
+        onClick={openOptionsDropdown}
+      >
+        <img src={isLightColor ? DropdownArrowBlackIcon : DropdownArrowWhiteIcon} />
+      </button>
+      <ActionDropdown
+        btnRef={optionDropdownBtnRef}
+        isOpen={isOptionDropdownOpen}
+        onClose={closeOptionsDropdown}
+        items={options}
+      />
+    </div>
+  );
+};
+
+export default LibraryTitle;
